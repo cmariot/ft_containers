635e40e1b96b8868f5597619dd79565e8e1909ad
Charles Mariot <contact@charles-mariot.fr>
2022-06-27 19:58:39 +0200

    En route pour les reverse_iterators

---
+++ containers/reverse_iterator.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_iterator.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 19:30:01 by cmariot           #+#    #+#             */
+/*   Updated: 2022/06/27 19:58:24 by cmariot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REVERSE_ITERATOR_HPP
+# define REVERSE_ITERATOR_HPP
+
+# include <iostream>
+# include "iterator.hpp"
+
+namespace	ft
+{
+
+} ;
+
+#endif
