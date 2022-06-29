21f50c1a7faae607fd8a40cf0813edb213063166
Charles Mariot <contact@charles-mariot.fr>
2022-06-29 14:36:57 +0200

    En route pour les reverse_iterators

--- containers/reverse_iterator.hpp
+++ containers/reverse_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 19:30:01 by cmariot           #+#    #+#             */
-/*   Updated: 2022/06/27 19:58:24 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/06/28 08:26:25 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,91 @@
 
 namespace	ft
 {
+	template <class Iter>
+	class reverse_iterator
+	{
 
-} ;
+		Iter	current;
+
+		public :
+
+			
+			typedef ft::iterator_traits<iterator<ft::input_iterator_tag, Iter> >	traits;
+
+			//MEMBER TYPES
+				typedef  Iter								iterator_type;
+				typedef typename traits::iterator_category	iterator_category;
+				typedef typename traits::value_type			value_type;
+				typedef typename traits::difference_type	difference_type;
+				typedef typename traits::pointer			pointer;
+				typedef typename traits::reference			reference;
+
+			//MEMBER FUNCTIONS
+				//CONSTRUCTOR
+				constexpr explicit reverse_iterator(Iter itr): current(itr)
+				{
+					return ;
+				};
+				//OPERATOR =
+				reverse_iterator operator = (reverse_iterator & rhs)
+				{
+					reverse_iterator	tmp;
+
+					tmp.current = this->current;
+					return (tmp);
+				};
+				//BASE
+				//OPERATOR *
+				constexpr auto & operator * (void)
+				{
+					return *std::prev(current); // <== returns the content of prev
+				};
+				//OPERATOR ->
+				//OPERATOR []
+				//OPERATOR ++
+				constexpr auto& operator ++ (void)
+				{
+					--current;
+					return *this;
+				};
+				//OPERATOR ++ (INT)
+				constexpr auto& operator ++ (int)
+				{
+					--current;
+					return *this;
+				};
+				//OPERATOR +=
+				//OPERATOR +
+				//OPERATOR --
+				constexpr auto& operator -- (void)
+				{
+					--current;
+					return *this;
+				};
+				//OPERATOR -- (INT)
+				constexpr auto& operator -- (int)
+				{
+					--current;
+					return *this;
+				};
+				//OPERATOR -=
+				//OPERATOR -
+
+			//NON MEMBER FUNCTIONS
+				//OPERATOR ==
+				//OPERATOR !=
+				constexpr friend bool operator != (reverse_iterator<Iter> a, reverse_iterator<Iter> b)
+				{
+					return a.current != b.current;
+				};
+				//OPERATOR <
+				//OPERATOR <=
+				//OPERATOR >
+				//OPERATOR >=
+				//OPERATOR +
+				//OPERATOR -
+	};
+
+};
 
 #endif
