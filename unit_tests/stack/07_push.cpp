3c808e1630a5b6bd038cc76f75f915fd9971a243
Charles VM UBUNTU <contact@charles-mariot.fr>
2022-08-27 09:20:14 +0200

    AutoSave

--- unit_tests/stack/07_push.cpp
+++ unit_tests/stack/07_push.cpp
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 19:17:36 by cmariot           #+#    #+#             */
-/*   Updated: 2022/08/26 19:17:37 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/08/27 09:03:13 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,42 @@
 
 int	stack_push_tests(void)
 {
+	ft::stack<int>	ft_stack;
+	
+	if (ft_stack.size() != 0)
+		return (1);
+
+	ft_stack.push(1);
+	ft_stack.push(2);
+	ft_stack.push(3);
+	ft_stack.push(4);
+	ft_stack.push(5);
+
+	if (ft_stack.empty() == true)
+		return (1);
+
+	if (ft_stack.size() != 5)
+		return (1);
+
+	for (int i = 0 ; i != 5 ; ++i)
+		ft_stack.pop();
+
+
+	if (ft_stack.size() != 0 || ft_stack.empty() == false)
+		return (1);
+
+
+	for (size_t i = 0 ; i < 100 ; i++)
+	{
+		ft_stack.push(i);
+		if (ft_stack.top() != static_cast<int>(i))
+			return (1);
+		if (ft_stack.size() != i + 1)
+			return (1);
+	}
+
+	if (ft_stack.size() != 100)
+		return (1);
+	
 	return (0);
 }
--- unit_tests/stack/08_pop.cpp
+++ unit_tests/stack/08_pop.cpp
@@ -6,7 +6,7 @@
 /*   By: cmariot <cmariot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 19:17:20 by cmariot           #+#    #+#             */
-/*   Updated: 2022/08/26 19:17:27 by cmariot          ###   ########.fr       */
+/*   Updated: 2022/08/27 09:15:42 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 int	stack_pop_tests(void)
 {
+	ft::vector<int> ft_vector(5, 42);
+	
+	ft::stack<int>	ft_stack(ft_vector);
+
+	if (ft_stack.size() != 5)
+		return (1);
+
+	size_t	n = ft_stack.size();
+
+	while (ft_stack.empty() == false)
+	{
+		if (ft_stack.top() != 42)
+			return (1);
+		ft_stack.pop();
+		if (ft_stack.size() !=  --n)
+			return (1);
+	}
+
 	return (0);
 }
